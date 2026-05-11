c5a3ae9b3023a7af20e2e3c6f71546cc70c94786
Aribafatma <118280104+Aribafatma@users.noreply.github.com>
2026-05-11 20:20:29 +0530

    Add files via upload

---
+++ practise.ts
@@ -0,0 +1,28 @@
+class Rectangle extends Shape {
+  name = "Rectangle";
+
+  constructor(public width: number, public height: number) {
+    super();
+  }
+
+  // Implement the abstract method
+  calculateArea(): number {
+    return this.width * this.height;
+  }
+}
+
+// Another subclass implementing the abstract class
+class Circle extends Shape {
+  name = "Circle";
+
+  constructor(public radius: number) {
+    super();
+  }
+
+  // Implement the abstract method
+  calculateArea(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+ 
